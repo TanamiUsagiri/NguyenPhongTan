49df6adb431fe86c27c34d97be5d5422f276aa9e
TanamiUsagiri <tanamiusagiri@gmail.com>
2024-06-20 10:46:58 +0700

    Lesson10

--- Lesson10/npt-lesson10/src/Components/NptCategoryList.js
+++ Lesson10/npt-lesson10/src/Components/NptCategoryList.js
@@ -1,20 +1,38 @@
 import React from 'react'
 
-export default function NptCategoryList({renderNptCategories, onAddNew}) {
+export default function NptCategoryList({renderNptCategories, onAddNew, onNptDelete, onNptEdit}) {
   console.log("renderNptCategories: ", renderNptCategories);
-  let nptCategoryElement = renderNptCategories.map((nptCategory,index)=>{
+  let nptCategoryElement = renderNptCategories.map((nptCategory,index)=>{ 
     return (
       <tr key={index}>
         <th>{index+1}</th>
         <td>{nptCategory.nptId}</td>
         <td>{nptCategory.nptCategoryName}</td>
         <td>{nptCategory.nptCategoryStatus===true?"Hien thi":"Tam khoa"}</td>
+        <td>
+          <button className='btn btn-danger' onClick={()=>nptHandleDelete(nptCategory.nptId)}>Delete</button>
+          <button className='btn btn-success' onClick={()=>nptHandleEdit(nptCategory)}>Edit</button>
+        </td>
       </tr>
     )
   })
 
   const nptHandleAdd = () =>{
     onAddNew(true);
+  }
+
+  // Delete function
+  const nptHandleDelete = (nptId)=>{
+    if(window.confirm('Do you want to delete Category named '+nptId+' ?')){
+    console.log("Delete: ",nptId);
+    onNptDelete(nptId)
+    }else{
+
+    }
+  }
+
+  const nptHandleEdit = (nptCategory)=>{
+    onNptEdit(nptCategory);
   }
   return (
     <div>
